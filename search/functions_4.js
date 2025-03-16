a35d97f7b4e90048ddaf5e715870a21a1be1084c
Pavel Bibergal <pavelb@keewano.com>
2025-03-16 12:06:47 +0200

    New documentation version

--- search/functions_4.js
+++ search/functions_4.js
@@ -7,6 +7,7 @@ var searchData=
   ['reportinstallcampaign_4',['ReportInstallCampaign',['../class_keewano_s_d_k.html#ac3e1433b5be7b52e16886f9cc5e64cf5',1,'KeewanoSDK']]],
   ['reportitemsexchange_5',['ReportItemsExchange',['../class_keewano_s_d_k.html#a03af6ed00403dce0cad7dbcc44f88463',1,'KeewanoSDK.ReportItemsExchange(string exchangeLocation, Item[] from, Item[] to)'],['../class_keewano_s_d_k.html#a37d2e41f2089fd6693be14fd59e99391',1,'KeewanoSDK.ReportItemsExchange(string exchangeLocation, ReadOnlySpan&lt; Keewano.Item &gt; from, ReadOnlySpan&lt; Keewano.Item &gt; to)']]],
   ['reportitemsreset_6',['ReportItemsReset',['../class_keewano_s_d_k.html#a9e551800fbca5b7c84e61ac882fbe787',1,'KeewanoSDK']]],
-  ['reportwindowclose_7',['ReportWindowClose',['../class_keewano_s_d_k.html#a01e9a0872f73f6e758d6bb2b8182978b',1,'KeewanoSDK']]],
-  ['reportwindowopen_8',['ReportWindowOpen',['../class_keewano_s_d_k.html#aca10811bff16611511be5b985c61dd0f',1,'KeewanoSDK']]]
+  ['reportonboardingmilestone_7',['ReportOnboardingMilestone',['../class_keewano_s_d_k.html#aed72b3384a06c6a62ab37c4a281b4a93',1,'KeewanoSDK']]],
+  ['reportwindowclose_8',['ReportWindowClose',['../class_keewano_s_d_k.html#a01e9a0872f73f6e758d6bb2b8182978b',1,'KeewanoSDK']]],
+  ['reportwindowopen_9',['ReportWindowOpen',['../class_keewano_s_d_k.html#aca10811bff16611511be5b985c61dd0f',1,'KeewanoSDK']]]
 ];
