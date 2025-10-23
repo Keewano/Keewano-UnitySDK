1a6a03fc47599c6a79ccfec240b7986112e9f4f0
Pavel Bibergal <pavel@keewano.com>
2025-10-23 12:42:27 +0300

    Updated documentation for new sdk version

---
+++ structKeewano_1_1Item.js
@@ -0,0 +1,7 @@
+var structKeewano_1_1Item =
+[
+    [ "Item", "structKeewano_1_1Item.html#a5556bd6dff4ee0cfb34d439a99362075", null ],
+    [ "Item", "structKeewano_1_1Item.html#ae6f2e655ce1f8e0a5ba6ec3bb9289eee", null ],
+    [ "Count", "structKeewano_1_1Item.html#a68f5ac73f138b41748ff8bd4f9a8ba67", null ],
+    [ "UniqItemName", "structKeewano_1_1Item.html#a46c16d8bb6938632758d20d91fa174e7", null ]
+];
